1ed70593093d3a72e8c08ce49a7b422f44dd8c96
NikCoolMint <demnuts911@gmail.com>
2025-11-03 15:29:52 +0000

    Commit!

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,26 @@
 import streamlit as st
+import pandas as pd
+from datetime import datetime
 
-st.title('👋 Welcome to my website')
-st.write("I'm building this live in class!")
+# Page Config
+st.set_page_config(
+page_title ='Niks portofolio',
+page_icon = '🚀',
+layout = 'wide'
+)
+
+# Custom CSS (optional - for styling)
+st.set_page_config('''
+    <style>  
+        .main-header {font-size: 42px; font-weight: bold; text-align:center;}
+        .sub-header {font_size: 24px; text-align:center; color: #666;}
+                 </style>
+                  ''',unsafe_allow_html = True)
+
+
+#Sidebar
+st.sidebar.title('📍Navigation')
+page = st.sidebar.radio('Go to',
+                        ['🏡 Home', '🚶🏾‍♂️ About', '📁 Porjects', '🔧 Skills', '📝 Resume', '📩 Contact'])
+                        
+                 
