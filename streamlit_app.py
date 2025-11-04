aaf9cfcefbab047b2dd1034b6e6502b63840f3bf
NikCoolMint <demnuts911@gmail.com>
2025-11-04 14:47:41 -0500

    Yeah yeh

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,23 +4,23 @@ from datetime import datetime
 
 # Page Config
 st.set_page_config(
-page_title ='Niks portofolio',
-page_icon = '🚀',
-layout = 'wide'
+  page_title ='Niks portofolio',
+  page_icon = '🚀',
+  layout = 'wide'
 )
 
 # Custom CSS (optional - for styling)
 st.set_page_config('''
-    <style>  
-        .main-header {font-size: 42px; font-weight: bold; text-align:center;}
-        .sub-header {font_size: 24px; text-align:center; color: #666;}
-                 </style>
-                  ''',unsafe_allow_html = True)
+                        <style>  
+                            .main-header {font-size: 42px; font-weight: bold; text-align:center;}
+                            .sub-header {font_size: 24px; text-align:center; color: #666;}
+                        </style>
+                     ''',unsafe_allow_html = True)
 
 
 #Sidebar
 st.sidebar.title('📍Navigation')
 page = st.sidebar.radio('Go to',
-                        ['🏡 Home', '🚶🏾‍♂️ About', '📁 Porjects', '🔧 Skills', '📝 Resume', '📩 Contact'])
+                        ['🏡 Home', '🚶🏾‍♂️ About', '📁 Projects', '🔧 Skills', '📝 Resume', '📩 Contact'])
                         
                  
