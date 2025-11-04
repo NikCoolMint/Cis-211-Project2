338ba839c5bf898c431c35ebf51ee8b641122f98
NikCoolMint <demnuts911@gmail.com>
2025-11-04 15:08:59 -0500

    Colums fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ if page == '🏡 Home':
   st.markdown('<p class="sub-header">Aspiring Tech Genius | Medgar Evers College</p>', unsafe_allow_html=True)
 
 # Three Columns for stats
-col1, col2, col3 = st.collumns(3)
+col1, col2, col3 = st.columns(3)
 
   with col1:
       st.metric('GPA', '3.8', '📚')
